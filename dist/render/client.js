0735fd674d71d8ade87a753c820787c74df19e16
misund <thomas@hemmeligadresse.com>
2016-08-18 17:03:11 +0200

    Build

--- dist/render/client.js
+++ dist/render/client.js
@@ -173,7 +173,6 @@ module.exports = function () {
      * Pull the state we need
      * for rendering our app
      */
-
     var _store$getState3 = store.getState();
 
     var _store$getState3$vers = _store$getState3.version;
@@ -277,7 +276,6 @@ module.exports = function () {
     /**
      * Destructure what we need from the state
      */
-
     var _store$getState4 = store.getState();
 
     var _store$getState4$pagi = _store$getState4.pagination;
@@ -414,7 +412,6 @@ module.exports = function () {
     /**
      * Pull out the modules list of the current config from redux state
      */
-
     var _ref4 = store.getState() || {};
 
     var _ref4$config = _ref4.config;
