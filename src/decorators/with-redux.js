becd4b1a16a3a3c60f6de0d4a81fb0f02e485590
misund <thomas@hemmeligadresse.com>
2017-03-23 13:53:01 +0100

    Make debugging great again

--- src/decorators/with-redux.js
+++ src/decorators/with-redux.js
@@ -1,6 +1,4 @@
-/**
- * Dependencies
- */
+// Dependencies
 import React from 'react';
 import hoistStatics from 'hoist-non-react-statics';
 import { createStore, applyMiddleware, compose } from 'redux';
@@ -8,26 +6,23 @@ import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
 import { Provider } from 'react-redux';
 
+import getDisplayName from '../utils/get-display-name';
+
 /**
- * Higher order component factory
- * for adding Aurora data fetching
+ * Higher order component factory for adding Aurora data fetching.
  */
 module.exports = function({
   reducer = () => console.log('No reducer supplied'),
   initialState = {},
 }) {
-  /**
-   * Create a logger (and default to pass-through if not browser)
-   */
+  // Create a logger (and default to pass-through if not browser)
   const logger = typeof window === 'object' ? (
     createLogger()
   ) : (
     () => next => action => next(action)
   );
 
-  /**
-   * Create developer tools middleware
-   */
+  // Create developer tools middleware
   const devTools = (
     typeof window === 'object' &&
     typeof window.devToolsExtension !== 'undefined'
@@ -37,51 +32,33 @@ module.exports = function({
     (f) => f
   );
 
-  /**
-   * Compose a store creator function with middleware
-   */
+  // Compose a store creator function with middleware
   const finalCreateStore = compose(
     applyMiddleware(thunk, logger),
     devTools
   )(createStore);
 
-  /**
-   * Create a Redux store
-   */
+  // Create a Redux store
   const makeStore = finalCreateStore.bind(null, reducer, initialState);
 
-  /**
-   * Return a function that extends
-   * a component with redux state handling
-   */
+  // Return a function that extends a component with redux state handling
   return function(Component) {
-    /**
-     * Create a component that wraps
-     * our input component in a Redux <Provider>
-     */
+    // Create a component that wraps our input component in a Redux <Provider>
     const withRedux = React.createClass({
-      /**
-       * Add a specific display name
-       */
-      displayName: 'withRedux',
+      // Add a specific display name
+      displayName: `${getDisplayName(Component)}WithRedux`,
 
-      /**
-       * Create a new Redux store for
-       * each component instance,
-       * to avoid sharing state between instances
-       */
+
+      // Create a new Redux store for each component instance.
+      // This avoids sharing state between instances
       getInitialState() {
         return {
           store: makeStore()
         };
       },
 
-      /**
-       * Render the component wrapped
-       * in a Redux <Provider> to expose
-       * the store and give children the
-       * ability to connect()
-       */
+      // Render the component wrapped in a Redux <Provider>.
+      // This exposes the store and gives children the ability to connect()
       render() {
         return (
           <Provider store={this.state.store}>
@@ -91,11 +68,7 @@ module.exports = function({
       }
     });
 
-    /**
-     * Return a decorated component
-     * with all the existing static
-     * methods hoisted
-     */
+    // Return a decorated component with all the existing static methods hoisted
     return hoistStatics(withRedux, Component);
   };
 };
