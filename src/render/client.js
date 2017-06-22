10e5badf1a9f653460ff096b968da8f433a076bc
misund <thomas@hemmeligadresse.com>
2017-06-22 18:41:49 +0200

    Upgrade webpack to 3.0.0

--- src/render/client.js
+++ src/render/client.js
@@ -330,4 +330,4 @@ export default function renderClient({
   .then(() => {
     infiniteScroll(loadMore);
   });
-};
+}
--- src/render/server.js
+++ src/render/server.js
@@ -248,4 +248,4 @@ export default function renderServer({
       })
     ;
   }
-};
+}
